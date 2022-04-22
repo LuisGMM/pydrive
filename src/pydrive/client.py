b1f028a43ec1b57a62562904ffd5eaab31dfc9bf
LuisGMM <emaildemiscuentas@gmail.com>
2022-04-22 21:27:42 +0200

    Update client.py

--- src/pydrive/client.py
+++ src/pydrive/client.py
@@ -1,3 +1,15 @@
 import socket
 
 
+PORT = 5050
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+
+HEADER = 64
+FORMAT  = 'utf-8'
+DISCONNECT = 'DISCONNECT'
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+
