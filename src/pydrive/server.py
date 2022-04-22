c400f2b3b61739172a97c03b5cec60351c9df8c2
LuisGMM <emaildemiscuentas@gmail.com>
2022-04-22 21:26:58 +0200

    Update server.py

--- src/pydrive/server.py
+++ src/pydrive/server.py
@@ -1,3 +1,16 @@
 import socket
 import threading
 
+PORT = 5050
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+
+HEADER = 64
+FORMAT  = 'utf-8'
+DISCONNECT = 'DISCONNECT'
+RECEIVED = 'RECEIVED'
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+
