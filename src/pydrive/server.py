416d40d40932575345514cba3fef2c348611de21
LuisGMM <emaildemiscuentas@gmail.com>
2022-04-22 21:27:06 +0200

    Update server.py

--- src/pydrive/server.py
+++ src/pydrive/server.py
@@ -14,3 +14,28 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 
 
+def handle_client(conn, addr):
+    print(f'New connection {addr} .')
+    connected: bool = True
+
+    while connected:
+
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+
+        if not msg_length:
+            continue
+        
+        msg_length = int(msg_length)
+        msg = conn.recv(msg_length).decode(FORMAT)
+
+        if msg == DISCONNECT:
+            connected = False
+            print(f'{addr} disconnected.')
+            continue
+
+        print(f'{addr} {msg}.')
+        conn.send(RECEIVED.encode(FORMAT))
+
+    conn.close()
+
+
