bcbd51c4b57c96908408b757d0b1dcb9e3705ae9
LuisGMM <emaildemiscuentas@gmail.com>
2022-04-23 09:06:24 +0200

    Update client.py

--- src/pydrive/client.py
+++ src/pydrive/client.py
@@ -11,29 +11,6 @@ HEADER = 64
 FORMAT  = 'utf-8'
 DISCONNECT = 'DISCONNECT'
 
-# client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# client.connect(ADDR)
-
-
-# def send(msg):
-    
-#     message = msg.encode(FORMAT)
-#     message_length = len(message)
-#     send_length = str(message_length).encode(FORMAT)
-#     send_length += b' ' * (HEADER - len(send_length))
-
-#     client.send(send_length)
-#     client.send(message)
-#     print(client.recv(2048))
-
-
-
-# send('Hello World')
-# send('Hello World')
-# send('Hello World')
-
-# send(DISCONNECT)
-
 
 class user(socket.socket):
 
