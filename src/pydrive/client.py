b44b56a6db483bdd3193fdebaabda3e2f0d1c01b
LuisGMM <emaildemiscuentas@gmail.com>
2022-04-23 08:24:40 +0200

    Update client.py

--- src/pydrive/client.py
+++ src/pydrive/client.py
@@ -48,5 +48,19 @@ class user(socket.socket):
     
         self.addr = addr
 
+
     def connect(self) -> None:
         super().connect(self.addr)
+
+
+    def send(self, msg):
+        
+        message = msg.encode(FORMAT)
+        message_length = len(message)
+        
+        send_length = str(message_length).encode(FORMAT)
+        send_length += b' ' * (HEADER - len(send_length))
+
+        client.send(send_length)
+        client.send(message)
+        print(client.recv(2048))
