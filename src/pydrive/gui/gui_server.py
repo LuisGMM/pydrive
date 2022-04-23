ab604cc7b2db98c75833a18ffa4bc9761ef84f42
LuisGMM <emaildemiscuentas@gmail.com>
2022-04-23 13:02:59 +0200

    Update gui_server.py

--- src/pydrive/gui/gui_server.py
+++ src/pydrive/gui/gui_server.py
@@ -11,3 +11,8 @@ class TreeDirectory(ttk.Frame):
     def __init__(self, main_window: ttk.Frame) -> None:
         super().__init__(main_window)
 
+        self.treeview: ttk.Treeview = ttk.Treeview(self)
+        self.treeview.grid(row=0, column=0, sticky="nsew")
+
+        self.treeview.tag_bind("fstag", "<<TreeviewOpen>>", self.event_item_opened)
+
