8fe4854770626fb9eae692e8532bad6ea5981c19
LuisGMM <emaildemiscuentas@gmail.com>
2022-04-23 21:58:08 +0200

    Update main.py

--- src/pydrive/main.py
+++ src/pydrive/main.py
@@ -1,11 +1,12 @@
 
 import tkinter as tk
 
-from gui import create_server
+from gui import create_server, gui_filemanager
 
+# ROOT = tk.Tk()
+# ROOT.geometry("600x300")
+# APP = create_server.CreateServerFrame(parent=ROOT)
+# APP.mainloop()
+# ROOT.destroy()
 
-ROOT = tk.Tk()
-ROOT.geometry("600x300")
-APP = create_server.CreateServerFrame(parent=ROOT)
-APP.mainloop()
-ROOT.destroy()
+gui_filemanager.main()
