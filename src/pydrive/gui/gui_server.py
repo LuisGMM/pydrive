cc980f7b59f4f0372457ed7446312d7ea738608b
LuisGMM <emaildemiscuentas@gmail.com>
2022-04-23 13:02:46 +0200

    Update gui_server.py

--- src/pydrive/gui/gui_server.py
+++ src/pydrive/gui/gui_server.py
@@ -8,4 +8,6 @@ from os.path import isdir, join, abspath
 
 class TreeDirectory(ttk.Frame):
 
-    ... 
+    def __init__(self, main_window: ttk.Frame) -> None:
+        super().__init__(main_window)
+
