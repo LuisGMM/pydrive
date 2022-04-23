8d1dfc9d191e90c38ed856d81bc6319cbba443a8
LuisGMM <emaildemiscuentas@gmail.com>
2022-04-23 09:06:33 +0200

    Update server.py

--- src/pydrive/server.py
+++ src/pydrive/server.py
@@ -13,46 +13,7 @@ FORMAT  = 'utf-8'
 DISCONNECT = 'DISCONNECT'
 RECEIVED = 'RECEIVED'
 
-# server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# server.bind(ADDR)
 
-
-# def handle_client(conn, addr):
-#     print(f'New connection {addr} .')
-#     connected: bool = True
-
-#     while connected:
-
-#         msg_length = conn.recv(HEADER).decode(FORMAT)
-
-#         if not msg_length:
-#             continue
-        
-#         msg_length = int(msg_length)
-#         msg = conn.recv(msg_length).decode(FORMAT)
-
-#         if msg == DISCONNECT:
-#             connected = False
-#             print(f'{addr} disconnected.')
-#             continue
-
-#         print(f'{addr} {msg}.')
-#         conn.send(RECEIVED.encode(FORMAT))
-
-#     conn.close()
-
-
-# def start():
-
-#     server.listen()
-#     print(f'Server is listening on {SERVER}')
-#     while True:
-
-#         conn, addr = server.accept()
-#         thread = threading.Thread(target=handle_client, args=(conn, addr))
-#         thread.start()
-
-#         print(f'Active connections {threading.activeCount() - 1}')
 
 
 
