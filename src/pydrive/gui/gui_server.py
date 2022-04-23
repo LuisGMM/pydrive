1f283a29d00791b279e17bc1270effc1d341b614
LuisGMM <emaildemiscuentas@gmail.com>
2022-04-23 17:27:06 +0200

    Update gui_server.py

--- src/pydrive/gui/gui_server.py
+++ src/pydrive/gui/gui_server.py
@@ -10,4 +10,16 @@ class CreateServerFrame(tk.Frame):
 
     def __init__(self, parent: tk.Tk) -> None:
 
-        ...
+        tk.Frame.__init__(self, parent)
+        self.parent = parent
+
+        self.parent.title("Create server")
+        self.parent.resizable(False, False)
+
+        server_name_label = tk.Label(self.parent, text="Server Name:", width=20)
+        server_name_label.grid(column=0, row=0)
+
+        self.server_name_entry = tk.Entry()
+        self.server_name_entry.grid(column=1, row=0)
+
+        
