8636cd67a9f9d4149cc212a1adfda9456e4bd200
LuisGMM <emaildemiscuentas@gmail.com>
2022-04-23 19:24:03 +0200

    Update window_manager.py

--- src/pydrive/gui/window_manager.py
+++ src/pydrive/gui/window_manager.py
@@ -1,14 +1,23 @@
 
 import tkinter as tk
 
+from os import listdir, sep
+from os.path import abspath, isdir, join
 
-class WindowManager(tk.Frame):
+from tkinter import ttk
 
-    def __init__(self, parent: tk.Tk, path: str, *args, **kwargs) -> None:
+class WindowManager(ttk.Frame):
 
-        tk.Frame.__init__(self, parent, *args, **kwargs)
+    def __init__(self, parent: ttk.Frame, path: str = None, *args, **kwargs) -> None:
+
+        super().__init__(parent, *args, **kwargs)
         self.parent = parent
+
+        self.grid(row=0, column=0, sticky='nsew')
 
 
 if __name__ == '__main__':
-    pass
+    
+    main_window = tk.Tk()
+    app = WindowManager(main_window)
+    app.mainloop()
