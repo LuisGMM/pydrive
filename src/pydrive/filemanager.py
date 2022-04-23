06ec075045e2b7d1718c91659dddb52b9a9d2939
LuisGMM <emaildemiscuentas@gmail.com>
2022-04-23 09:28:56 +0200

    Update filemanager.py

--- src/pydrive/filemanager.py
+++ src/pydrive/filemanager.py
@@ -1,7 +1,15 @@
 
+from typing import List, Union
+
+from __future__ import annotations
 
 class Path():
-    raise NotImplementedError()
+    
+    def __init__(self, parent: Path , children: List[Path], contains: Union[File, Folder] ) -> None:
+        
+        self.parent = parent
+        self.children = children
+        self.contains = contains
 
 
 class File():
