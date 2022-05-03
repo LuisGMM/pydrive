a46b38f9555898033cbdef0d8275e0cedec5dd37
LuisGMM <emaildemiscuentas@gmail.com>
2022-05-03 17:12:11 +0200

    Update fileexplorer.py

--- src/pydrive/gui/fileexplorer.py
+++ src/pydrive/gui/fileexplorer.py
@@ -1,7 +1,7 @@
 
 import tkinter as tk
 
-from pydrive.gui.gui_filemanager import WindowManager
+from pydrive.gui.windowmanager import WindowManager
 from pydrive.gui.treedirectory import TreeDirectory
 
 
